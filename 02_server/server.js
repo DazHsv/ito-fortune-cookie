48a2c34b78ce65d5f7bf759b94390b4bfffd11a6
Hugo Sanchez V <programacionhsv@gmail.com>
2016-10-19 19:33:22 -0500

    Servidor basado en URL

--- 02_server/server.js
+++ 02_server/server.js
@@ -5,7 +5,10 @@ const fs = require("fs");
 const config = require("./config/config");
 
 var handler = function(req,res) {
-  fs.readFile(__dirname + "/index.html",function(err, data){
+  console.log(req.url);
+  let url = req.url;
+  if(url == "/index" || url == "/index.html"){
+    fs.readFile(__dirname + "/index.html",function(err, data){
     if(err) console.log(err);
     res.writeHead(200,{
       "Content-Type":"text/html",
@@ -14,9 +17,13 @@ var handler = function(req,res) {
     res.write(data);
     res.end();
   });
+  } else {
+    res.write("En contrucción.");
+    res.end();
+  }
 };
 const server = http.createServer(handler);
 
-server.listen(8080, function() {
+server.listen(config.port, config.ip, function() {
   console.log(`Server running @ http://${config.ip}:${config.port}/`);
 });
